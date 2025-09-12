98d20f1b8d6c625bf8aab4d4df657eea5007b196
Codster177 <codster157@gmail.com>
2025-09-12 11:38:16 -0400

    UserId Fix

--- js/mainpage.js
+++ js/mainpage.js
@@ -2,7 +2,7 @@ function onMainpageLoad() {
     let userInfo = Global.readCookie();
     let firstName = userInfo.firstname;
     let lastName = userInfo.lastname;
-    let userId = userInfo.userId;
+    let userId = userInfo.userid;
 
     let welcomeLabel = document.getElementById("welcomeLabel");
     welcomeLabel.innerHTML = "Welcome " + firstName + " " + lastName + ", ID: " + userId;
