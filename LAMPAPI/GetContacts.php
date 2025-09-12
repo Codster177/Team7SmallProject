ed4e81b23a0fb7a35131912fa33c5747fd79c7ca
Christian <104219430+chrisgw4@users.noreply.github.com>
2025-09-12 15:34:33 -0400

    Added Get Contacts API
    It will get the contacts of the associated User Id and will return a list of objects of the contacts. Returns them in FirstName Alphabetical Order

---
+++ LAMPAPI/GetContacts.php
@@ -0,0 +1,95 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("farrukhanwar.site", "APIDevs", "8CYS0b3ltgKP58IpcyqI", "SmallProject");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		// Select all contact information that is connected to the passed in UserID
+		$stmt = $conn->prepare("select FirstName, LastName, Phone, Email, UserID, ID, CreatedAt from Contacts where UserID=?
+		ORDER BY FirstName
+		 
+		"); // LIMIT the number of contacts retrieved by the passed in limit from JSON
+		
+		// Need to make it so it grabs based on letters of contacts since ID will be shuffled since it is sorted by firstname
+		
+		// Bind the passed in UserID to the command
+		$stmt->bind_param("s", $inData["userId"]);
+		$stmt->execute(); // Execute the command
+		
+		$result = $stmt->get_result();
+		
+		// Create an array that will store the User information that will be sent back
+		$array = [];
+		
+		// Loop through the results up to a total of 30 contacts
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			
+			
+			// Create a new object to store Contact Information
+			$obj = new stdClass();
+			// Add each element necessary for the object
+			$obj->UserID = $row["UserID"];
+			$obj->ContactId = $row["ID"];
+			$obj->FirstName = $row["FirstName"];
+			$obj->LastName = $row["LastName"];
+			$obj->PhoneNumber = $row["Phone"];
+			$obj->Email = $row["Email"];
+			$obj->CreatedAt = $row["CreatedAt"];
+			// Add the Contact object to the array
+			$array[$searchCount] = $obj;
+		}
+		
+		// Convert the Contact array into JSON
+		$myJSON = json_encode($array);
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Contacts Found" );
+		}
+		else
+		{
+			returnWithInfo( $myJSON );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
